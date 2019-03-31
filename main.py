0cc19c9060a7df953edf1667174bcae81df3fe7b
Tatsuya <ted@Tatsuyas-MacBook-Pro.local>
2019-03-31 16:22:23 -0400

    fixed invalid codes

--- main.py
+++ main.py
@@ -44,8 +44,5 @@ def handle_message(event):
 
 
 if __name__ == '__main__':
-    port = int(os.geteenv("PORT", 5000))
+    port = int(os.getenv("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
-
-hero conf

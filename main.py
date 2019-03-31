13e192c70abab4e18b499e9ee6e8b2ca2b450d23
Tatsuya <ted@Tatsuyas-MacBook-Pro.local>
2019-03-31 16:31:51 -0400

    debug on

--- main.py
+++ main.py
@@ -45,4 +45,4 @@ def handle_message(event):
 
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)

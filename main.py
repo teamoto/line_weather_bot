cb5fa7d9893d922aba8286ac6fa8ae51714d5101
Tatsuya <ted@Tatsuyas-MacBook-Pro.local>
2019-04-01 20:28:45 -0400

    Added error handling

--- main.py
+++ main.py
@@ -30,35 +30,53 @@ base_url += '/' + DARK_SKY_API_KEY
 option = "exclude=currently,minutely,hourly,alerts&amp;units=si"
 
 
+# define a function to return latitude and longitude
+def get_geo_location('location'):
+    """
+    Get location info throuh geopy.
+    This function returns latitude and longitude in a dictionary
+    """
+    geo_code = Nominatim().geocode(str(location.strip()), language='en-US')
+    if geo_code is not None:
+        latitude = str(geo_code.latitude)
+        longitude = str(geo_code.longitude)
+        return {
+            'latitude': latitude,
+            'longitude': longitude
+        }
+    return None
+
+
 # define function to get weather info
 def get_weather(base_url, option, location):
-    # get location info throuh geopy
-    geo_code = Nominatim().geocode(str(location.strip()), language='en-US')
-    latitude = str(geo_code.latitude)
-    longitude = str(geo_code.longitude)
-    # set date info
-    d = datetime.today().strftime('%Y-%m-%d')
-    search_date = d + 'T00:00:00'
-    # create a request url
-    request_url = base_url + '/' + latitude + ',' + longitude
-    request_url += ',' + search_date + '?' + option
-    # query a request to get wather informaiton
-    r = requests.get(request_url)
-    # get result in a json format
-    json_res = r.json()
-    # specify condition to set temp unit
-    unit_type = '°F' if json_res['flags']['units'] == 'us' else '°C'
-    # organize result
-    weather = str(json_res['daily']['data'][0]['summary'])
-    temp_max = str(json_res['daily']['data'][0]['apparentTemperatureMax'])
-    temp_min = str(json_res['daily']['data'][0]['apparentTemperatureMin'])
-    # return a result as a dictionary
-    return {
-        'location': geo_code.address,
-        'weather': weather,
-        'temp_max': (temp_max+unit_type),
-        'temp_min': (temp_min+unit_type)
-    }
+    # Get weather informaiton
+    # get locaiton info (lati + long)
+    loc = get_geo_location(location)
+    if loc is not None:
+        # set date info
+        d = datetime.today().strftime('%Y-%m-%d')
+        search_date = d + 'T00:00:00'
+        # create a request url
+        request_url = base_url + '/' + loc['latitude'] + ',' + loc['longitude']
+        request_url += ',' + search_date + '?' + option
+        # query a request to get wather informaiton
+        r = requests.get(request_url)
+        # get result in a json format
+        json_res = r.json()
+        # specify condition to set temp unit
+        unit_type = '°F' if json_res['flags']['units'] == 'us' else '°C'
+        # organize result
+        weather = str(json_res['daily']['data'][0]['summary'])
+        temp_max = str(json_res['daily']['data'][0]['apparentTemperatureMax'])
+        temp_min = str(json_res['daily']['data'][0]['apparentTemperatureMin'])
+        # return a result as a dictionary
+        return {
+            'location': geo_code.address,
+            'weather': weather,
+            'temp_max': (temp_max+unit_type),
+            'temp_min': (temp_min+unit_type)
+        }
+    return None
 
 
 @app.route('/callback', methods=['POST'])
@@ -83,10 +101,13 @@ def callback():
 def handle_message(event):
     location = event.message.text
     data = get_weather(base_url, option, location)
-    reply_text = f"Location: {data['location']}\n"
-    reply_text += f"Weather: {data['weather']}\n"
-    reply_text += f"Max Temp: {data['temp_max']}\n"
-    reply_text += f"Min Temp: {data['temp_min']}\n"
+    if data is not None:
+        reply_text = f"Location: {data['location']}\n"
+        reply_text += f"Weather: {data['weather']}\n"
+        reply_text += f"Max Temp: {data['temp_max']}\n"
+        reply_text += f"Min Temp: {data['temp_min']}\n"
+    else:
+        reply_text = f'{location} does not exist!!'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply_text)
